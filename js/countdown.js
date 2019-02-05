9e6c241f0e461c53533e442c2aac527d6a5aa17b
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2019-02-04 23:07:12 -0500

    fixed countdown timer

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-let countDownDate = new Date("April 27, 2019 00:09:00").getTime();
+let countDownDate = new Date("April 27, 2019 14:00:00").getTime();
 
 // Update the count down every 1 second
 let x = setInterval(function () {
